1b50147acf9fd2d18ad4905d760aa361ce9ba1d0
BROWN-dot-tech <fokamsteve96@gmail.com>
2023-08-23 16:35:38 +0200

    first modification

---
+++ config.php
@@ -0,0 +1,18 @@
+
+<?php
+
+$servername = 'localhost:3310';
+$username = 'root';
+$password = '';
+$dbname = 'restate';
+/*attemp to connect to mysql database*/
+$con = mysqli_connect($servername, $username, $password,$dbname);
+if(!$con)
+{
+    echo 'cannot connect to the database';
+}
+/*testing if connected.*/
+else{
+    echo 'successful';
+}
+?>
